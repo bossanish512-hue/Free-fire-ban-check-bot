406e1fbef64b0b08f02717a1e4245a1813b90bda
bossanish512-hue <bossanish512@gmail.com>
2025-08-24 11:24:12 +0545

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import aiohttp
 import json
 import os
 from datetime import datetime, timedelta, timezone
+import discord
+from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
